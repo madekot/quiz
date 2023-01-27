cfd6381bbe81991516973381c2c091fc7c986662
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-01-27 10:00:54 +0300

    справляет ошибки линтинга

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import {ChakraProvider, extendTheme, withDefaultColorScheme} from '@chakra-ui/react'
-import './index.module.scss';
-import {App} from './components/App';
-import {Provider} from "react-redux";
-import {store} from "./store";
+import './index.module.scss'
+import {App} from './components/App'
+import {Provider} from 'react-redux'
+import {store} from './store'
 
 const customTheme = extendTheme(
   withDefaultColorScheme({
-    colorScheme: 'green'
+    colorScheme: 'green',
   }),
   withDefaultColorScheme({
     colorScheme: 'gray',
     components: ['Table'],
-  }),
+  })
 )
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -27,4 +27,4 @@ root.render(
       </ChakraProvider>
     </Provider>
   </React.StrictMode>
-);
+)
