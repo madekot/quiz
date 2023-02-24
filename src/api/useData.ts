1d9cff081d1045087f4c1fb2b86ec07cbcbfc217
Mikhail Golubtsov <mgolubcov@gmail.com>
2023-02-24 14:55:40 +0300

    исправление по пулреквесту Feature

---
+++ src/api/useData.ts
@@ -0,0 +1,12 @@
+import {useEffect, useState} from 'react';
+import {Question} from '../types';
+
+export const useData = (getData: () => Promise<Question[]>) => {
+  const [data, setData] = useState<Question[]>([]);
+
+  useEffect(() => {
+    getData().then((response) => {return setData(response);});
+  }, [getData]);
+
+  return data;
+};
